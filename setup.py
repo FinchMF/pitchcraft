6123f4daaef0143b08d797a27655733733bfb592
Matthew Finch <finchrmatthew@gmail.com>
2021-04-03 02:14:00 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup
+
+setup(
+    package_dir={"":"/"},
+    name='core',
+    description='signal and frequency system generator',
+    author='Matthew Finch',
+    author_emails='finchrmatthew@gmail.com',
+    packages=['core'],
+    install_requires=[
+        'dash==1.19.0',
+        'dash-core-components==1.15.0',
+        'dash-cytoscape==0.2.0',
+        'dash-html-components==1.1.2',
+        'dash-renderer==1.9.0',
+        'dash-table==4.11.2',
+        'matplotlib==3.1.3',
+        'gunicorn==20.0.4',
+        'numpy==1.18.0',
+        'scipy==1.4.1'
+    ],
+    zip_safe=False
+)
